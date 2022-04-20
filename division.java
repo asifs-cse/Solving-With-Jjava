2cb1b2586220e62d48838c0c67bc2ac7b727f261
asifs-cse <asifs.cse@gmail.com>
2022-04-20 23:39:40 +0530

    multiplication

---
+++ division.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class division {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int a,b,div;
+        a= in.nextInt();
+        b= in.nextInt();
+
+        div= a/b;
+        
+        System.out.println(div);
+        
+    }
+}
---
+++ multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class multiplication {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int a,b,mul;
+        a= in.nextInt();
+        b= in.nextInt();
+
+        mul= a*b;
+        
+        System.out.println(mul);
+        
+    }
+}
